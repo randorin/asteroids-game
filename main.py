e4823a9d06b3d1171ce47039d10cbba71976bf18
randorin <erinseaweed@gmail.com>
2025-01-02 22:37:12 -0500

    fill screen with black color using pygame

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"{SCREEN_WIDTH}")
-    print(f"{SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
