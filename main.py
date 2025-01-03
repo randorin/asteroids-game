9af1b4794e93bef53a19511f72d324f7c67488b9
randorin <erinseaweed@gmail.com>
2025-01-03 18:59:19 -0500

    add player.py and circleshape.py

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,9 @@
+import pygame
+# in the player class
+def triangle(self):
+    forward = pygame.Vector2(0, 1).rotate(self.rotation)
+    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+    a = self.position + forward * self.radius
+    b = self.position - forward * self.radius - right
+    c = self.position - forward * self.radius + right
+    return [a, b, c]
