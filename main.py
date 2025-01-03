f7100f7332f8837a6ebb1de768420db43c57809f
randorin <erinseaweed@gmail.com>
2025-01-02 22:12:28 -0500

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"{SCREEN_WIDTH}")
+    print(f"{SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
