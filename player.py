473c7ad4e0a9b6cd9a24e0bdd3cbd2e71852a72b
randorin <erinseaweed@gmail.com>
2025-01-13 21:20:59 -0500

    added rotation field in player.py

--- player.py
+++ player.py
@@ -1,4 +1,9 @@
 import pygame
+from CircleShape import *
+class Player (CircleShape):
+    def __init__(self, x, y):
+        super().__init__()
+        self.rotation = 0
 # in the player class
 def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
